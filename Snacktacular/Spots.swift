c1bc6bfab1f4de21b3265782a7d49883a4ac9419
Alberto Medina <albertomedina@macbook-pro-9.local>
2019-04-01 16:42:23 -0400

    9.2 Complete

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,12 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Alberto Medina on 4/1/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+class Spots {
+    var spotArray = [Spot]()
+}
